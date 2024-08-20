9f62eab2586da7ea84a0c9335319255306a20fa8
Habib <habibmakik1942@gmail.com>
2024-08-20 18:19:36 +0500

    fixed

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -13,114 +13,110 @@ const NavBar = () => {
   const [ThirdArrow, setThirdArrow] = useState(false);
   const [FourthArrow, setFourthArrow] = useState(false);
   const [open, setOpen] = useState(false);
-  
+
 
   return (
     <div className="border-b border-solid ">
       {width > 768 ? (
-       <div className="w-[90%] mx-auto flex justify-between items-center py-[20px]">
-       <div>
-         <p className="font-bold text-[28px] text-[#007bff]">
-           Name Idea Generator
-         </p>
-       </div>
-       <div className="flex gap-4">
-         <div
-           className="relative"
-           onMouseEnter={() => setFirstArrow(true)}
-           onMouseLeave={() => setFirstArrow(false)}
-         >
-           <div className="flex items-center hover:text-[#007bff] gap-[2px] cursor-pointer relative">
-           <a href="https://www.nameideagenerator.com/categories/fantasy-and-mythicalcreatures">  <p className="font-medium text-[14px] hover:border-b hover:border-solid">
-               Fantasy and Mythical Creatures
-             </p>
-             </a>
-             {firstarrow ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
-           </div>
-           <ul
-             className={`absolute text-center top-[100%] flex flex-col gap-4 pt-[20px] py-[10px] text-[14px] bg-white w-[100%] transition-all duration-300 ${
-               firstarrow ? "opacity-100 visible" : "opacity-0 invisible"
-             }`}
-           >
-             <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/demon-name-generator"> Demon Name <br /> Generator </a></li>
-             <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/tiefling-name-generator">Tiefling Name <br /> Generator</a></li>
-             <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/dragonborn-name-generator">Dragonborn Name <br /> Generator</a></li>
-             <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/orc-name-generator">Orc Name <br /> Generator</a></li>
-           </ul>
-         </div>
-         <div
-           className="relative"
-           onMouseEnter={() => setSecondArrow(true)}
-           onMouseLeave={() => setSecondArrow(false)}
-         >
-           <div className="flex items-center hover:text-[#007bff] gap-[2px] cursor-pointer relative">
-            <a href="https://www.nameideagenerator.com/categories/cultural-and-historical"> <p className="font-medium text-[14px] hover:border-b hover:border-solid">
-               Cultural and Historical
-             </p></a>
-             {SecondArrow ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
-           </div>
-           <ul
-             className={`absolute text-center top-[100%] flex flex-col gap-4 pt-[20px] py-[10px] text-[14px] bg-white w-[100%] transition-all duration-300 ${
-               SecondArrow ? "opacity-100 visible" : "opacity-0 invisible"
-             }`}
-           >
-             <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/viking-name-generator">Viking Name <br /> Generator</a></li>
-             <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/russian-name-generator">Russian Name <br /> Generator</a></li>
-             <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/greek-name-generator">Greek Name <br /> Generator</a></li>
-             <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/norse-name-generator">Norse Name <br /> Generator</a></li>
-           </ul>
-         </div>
-         <div
-           className="relative"
-           onMouseEnter={() => setThirdArrow(true)}
-           onMouseLeave={() => setThirdArrow(false)}
-         >
-           <div className="flex items-center hover:text-[#007bff] gap-[2px] cursor-pointer relative">
-          <a href="https://www.nameideagenerator.com/categories/fictional-and-scifi"> <p className="font-medium text-[14px] hover:border-b hover:border-solid">
-               Fictional and Sci-Fi
-             </p></a>  
-             {ThirdArrow ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
-           </div>
-           <ul
-             className={`absolute text-center top-[100%] flex flex-col gap-4 pt-[20px] py-[10px] text-[14px] bg-white w-[100%] transition-all duration-300 ${
-               ThirdArrow ? "opacity-100 visible" : "opacity-0 invisible"
-             }`}
-           >
-             <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/alien-name-generator">Alien Name <br /> Generator</a></li>
-             <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/island-name-generator">Robot Name <br /> Generator</a></li>
-             <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/saiyan-name-generator">Saiyan Name <br /> Generator</a></li>
-             <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/angel-name-generator">Angel Name <br /> Generator</a></li>
-           </ul>
-         </div>
-         <div
-           className="relative"
-           onMouseEnter={() => setFourthArrow(true)}
-           onMouseLeave={() => setFourthArrow(false)}
-         >
-           <div className="flex items-center hover:text-[#007bff] gap-[2px] cursor-pointer relative">
-             <p className="font-medium text-[14px] hover:border-b hover:border-solid">
-               Miscellaneous Themes
-             </p>
-             {FourthArrow ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
-           </div>
-           <ul
-             className={`absolute text-center top-[100%] flex flex-col gap-4 pt-[20px] py-[10px] text-[14px] bg-white w-[100%] transition-all duration-300 ${
-               FourthArrow ? "opacity-100 visible" : "opacity-0 invisible"
-             }`}
-           >
-             <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/island-name-generator">Island Name <br /> Generator</a></li>
-             <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/song-name-generator">Song Name <br /> Generator</a></li>
-             <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/angel-name-generator">Angel Name <br /> Generator</a></li>
-             <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/horse-name-generator">Horse Name <br /> Generator</a></li>
-           </ul>
-         </div>
-         <div className="flex items-center">
-           <p className="hover:text-[#007bff] font-medium text-[14px] cursor-pointer">
-             Contact us
-           </p>
-         </div>
-       </div>
-     </div>
+        <div className="w-[90%] mx-auto flex justify-between items-center py-[20px]">
+          <div>
+            <p className="font-bold text-[28px] text-[#007bff]">
+              Name Idea Generator
+            </p>
+          </div>
+          <div className="flex gap-4">
+            <div
+              className="relative"
+              onMouseEnter={() => setFirstArrow(true)}
+              onMouseLeave={() => setFirstArrow(false)}
+            >
+              <div className="flex items-center hover:text-[#007bff] gap-[2px] cursor-pointer relative">
+                <a href="https://www.nameideagenerator.com/categories/fantasy-and-mythicalcreatures">  <p className="font-medium text-[14px] hover:border-b hover:border-solid">
+                  Fantasy and Mythical Creatures
+                </p>
+                </a>
+                {firstarrow ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
+              </div>
+              <ul
+                className={`absolute text-center top-[100%] flex flex-col gap-4 pt-[20px] py-[10px] text-[14px] bg-white w-[100%] transition-all duration-300 ${firstarrow ? "opacity-100 visible" : "opacity-0 invisible"
+                  }`}
+              >
+                <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/demon-name-generator"> Demon Name <br /> Generator </a></li>
+                <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/tiefling-name-generator">Tiefling Name <br /> Generator</a></li>
+                <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/dragonborn-name-generator">Dragonborn Name <br /> Generator</a></li>
+                <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/orc-name-generator">Orc Name <br /> Generator</a></li>
+              </ul>
+            </div>
+            <div
+              className="relative"
+              onMouseEnter={() => setSecondArrow(true)}
+              onMouseLeave={() => setSecondArrow(false)}
+            >
+              <div className="flex items-center hover:text-[#007bff] gap-[2px] cursor-pointer relative">
+                <a href="https://www.nameideagenerator.com/categories/cultural-and-historical"> <p className="font-medium text-[14px] hover:border-b hover:border-solid">
+                  Cultural and Historical
+                </p></a>
+                {SecondArrow ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
+              </div>
+              <ul
+                className={`absolute text-center top-[100%] flex flex-col gap-4 pt-[20px] py-[10px] text-[14px] bg-white w-[100%] transition-all duration-300 ${SecondArrow ? "opacity-100 visible" : "opacity-0 invisible"
+                  }`}
+              >
+                <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/viking-name-generator">Viking Name <br /> Generator</a></li>
+                <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/russian-name-generator">Russian Name <br /> Generator</a></li>
+                <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/greek-name-generator">Greek Name <br /> Generator</a></li>
+                <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/norse-name-generator">Norse Name <br /> Generator</a></li>
+              </ul>
+            </div>
+            <div
+              className="relative"
+              onMouseEnter={() => setThirdArrow(true)}
+              onMouseLeave={() => setThirdArrow(false)}
+            >
+              <div className="flex items-center hover:text-[#007bff] gap-[2px] cursor-pointer relative">
+                <a href="https://www.nameideagenerator.com/categories/fictional-and-scifi"> <p className="font-medium text-[14px] hover:border-b hover:border-solid">
+                  Fictional and Sci-Fi
+                </p></a>
+                {ThirdArrow ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
+              </div>
+              <ul
+                className={`absolute text-center top-[100%] flex flex-col gap-4 pt-[20px] py-[10px] text-[14px] bg-white w-[100%] transition-all duration-300 ${ThirdArrow ? "opacity-100 visible" : "opacity-0 invisible"
+                  }`}
+              >
+                <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/alien-name-generator">Alien Name <br /> Generator</a></li>
+                <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/island-name-generator">Robot Name <br /> Generator</a></li>
+                <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/saiyan-name-generator">Saiyan Name <br /> Generator</a></li>
+                <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/angel-name-generator">Angel Name <br /> Generator</a></li>
+              </ul>
+            </div>
+            <div
+              className="relative"
+              onMouseEnter={() => setFourthArrow(true)}
+              onMouseLeave={() => setFourthArrow(false)}
+            >
+              <div className="flex items-center hover:text-[#007bff] gap-[2px] cursor-pointer relative">
+                <p className="font-medium text-[14px] hover:border-b hover:border-solid">
+                  Miscellaneous Themes
+                </p>
+                {FourthArrow ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
+              </div>
+              <ul
+                className={`absolute text-center top-[100%] flex flex-col gap-4 pt-[20px] py-[10px] text-[14px] bg-white w-[100%] transition-all duration-300 ${FourthArrow ? "opacity-100 visible" : "opacity-0 invisible"
+                  }`}
+              >
+                <li className="hover:text-[#007bff]"><Link href="https://www.nameideagenerator.com/island-name-generator">Island Name <br /> Generator</Link></li>
+                <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/song-name-generator">Song Name <br /> Generator</a></li>
+                <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/angel-name-generator">Angel Name <br /> Generator</a></li>
+                <li className="hover:text-[#007bff]"><a href="https://www.nameideagenerator.com/horse-name-generator">Horse Name <br /> Generator</a></li>
+              </ul>
+            </div>
+            <div className="flex items-center">
+              <p className="hover:text-[#007bff] font-medium text-[14px] cursor-pointer">
+                Contact us
+              </p>
+            </div>
+          </div>
+        </div>
       ) : (
         <div className="w-[90%] mx-auto flex justify-between items-center py-[20px]">
           <div>
