a0842bc792fc3004cec911fc11359d6e295f158e
Habib <habibmakik1942@gmail.com>
2024-08-25 17:50:46 +0500

    added

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+// Add this section to enable Google Analytics
+analytics: {
+    google: G-CR7P6GW9K1,
+  },
 };
 
 export default nextConfig;
