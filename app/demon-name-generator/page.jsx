74cbf8f767189bf3b70ad7df50f5d08e51df396b
Habib <habibmakik1942@gmail.com>
2024-09-13 21:09:21 +0500

    added

--- app/demon-name-generator/page.jsx
+++ app/demon-name-generator/page.jsx
@@ -27,11 +27,22 @@ export const metadata = {
 
   keywords: ['demon name generator', 'fantasy names', 'RPG names', 'demon names', 'unique demon names', 'female demon name generator', 'demon name generator male', 'random demon name generator', 'demon name generator female', 'hazbin hotel demon name generator', 'dnd demon name generator', 'fantasy demon name generator', 'biblical demon name generator', 'frost demon name generator'],
   
+  
   alternates:{
     canonical:"https://www.nameideagenerator.com/demon-name-generator"
   },
   
   robots:{index:true, follow: true},
+
+  schema: {
+    "@type": "WebApplication",
+    "name": "Demon Name Generator With Meaning | Devil Name Generator",
+    "description": "Create unique demon names with meanings using our demon name generator. Ideal for stories, games, and creative projects!",
+    "url": "https://www.nameideagenerator.com/demon-name-generator",
+    "image": "demonpic.jpg",
+    "applicationCategory": "Entertainment",
+    "operatingSystem": "Online",
+  },
 }
 
 const page = () => {
