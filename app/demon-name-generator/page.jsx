e662bcc9448505f341e6743b24f5f8cc5cb63a6b
Malik <habibmalik1942@gmail.com>
2025-02-21 15:54:06 +0500

    add

--- app/demon-name-generator/page.jsx
+++ app/demon-name-generator/page.jsx
@@ -1,4 +1,5 @@
-import DemonNameGenerator from "@/components/demon-name-generator/Demon-name-generator"
+import DemonNameGenerator from "@/components/demon-name-generator/Demon-name-generator";
+import Script from "next/script";
 
 export const metadata = {
   title: 'AI Demon Name Generator – Create Dark & Powerful Names Instantly',
@@ -43,11 +44,61 @@ export const metadata = {
 }
 
 const page = () => {
+  const schemaMarkup = {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    "name": "Demon Name Generator",
+    "url": "https://www.nameideagenerator.com/demon-name-generator",
+    "description":
+      "Generate eerie, AI-powered demon names with deep meanings. Perfect for stories, games, and more. Try our advanced AI name generator now and summon the perfect name! What will your demon be called?",
+    "image": "https://www.nameideagenerator.com/demonpic.jpg",
+    "keywords": [
+      "demon name generator ",
+      "fantasy names",
+      "RPG names",
+      "demon names",
+      "unique demon names",
+      "female demon name generator",
+      "demon name generator male",
+      "random demon name generator",
+      "demon name generator female",
+      "hazbin hotel demon name generator",
+      "dnd demon name generator",
+      "fantasy demon name generator",
+      "biblical demon name generator",
+      "frost demon name generator",
+    ],
+    "author": {
+      "@type": "Organization",
+      "name": "Name Idea Generator",
+      "url": "https://www.nameideagenerator.com",
+    },
+    "publisher": {
+      "@type": "Organization",
+      "name": "Name Idea Generator",
+      "url": "https://www.nameideagenerator.com",
+      "logo": {
+        "@type": "ImageObject",
+        "url": "https://www.nameideagenerator.com/Logo.png",
+      },
+    },
+    "brand": {
+      "@type": "Brand",
+      "name": "Name Idea Generator",
+      "logo": "https://www.nameideagenerator.com/Logo.png",
+    },
+  };
+
   return (
     <div>
-     <DemonNameGenerator />
+      {/* Inject Optimized Schema Markup */}
+      <Script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaMarkup) }}
+      />
+      <DemonNameGenerator />
     </div>
-  )
-}
+  );
+};
 
 export default page
