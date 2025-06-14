0dfc215ae5e2fbec6914026fc58265c3c37260e0
Malik <habibmalik1942@gmail.com>
2025-06-14 18:48:47 +0500

    add

--- app/layout.js
+++ app/layout.js
@@ -96,7 +96,23 @@ export default function RootLayout({ children }) {
           crossOrigin="anonymous"
         />
         {/* Inject homepage schema */}
-        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaMarkup) }} />
+        <Script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaMarkup) }} />
+
+
+      <Script
+  id="microsoft-clarity"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "rzdyimb72d");
+    `,
+  }}
+/>
+
 
       </head>
       <body className={inter.className}>
