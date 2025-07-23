0390e1fedda0866c98a151bd5c886e87d3ca8381
Malik <habibmalik1942@gmail.com>
2025-07-23 17:01:21 +0500

    add

--- app/blog/category/[category]/page.jsx
+++ app/blog/category/[category]/page.jsx
@@ -1,9 +1,22 @@
-import DynamicCategory from "../../../../components/Blog/DynamicCategory";
-import React from "react";
+// app/blog/category/[category]/page.js (or .tsx)
+import DynamicCategory from "@/components/Blog/DynamicCategory";
+import { Metadata } from "next";
 
-const page = async ({ params }) => {
-  const slug = (await params).category;
+export async function generateMetadata({ params }) {
+  const slug = params.category;
+
+  return {
+    title: `${slug.replace(/-/g, " ")} | Name Idea Generator`, // Optional: Set dynamic title
+    canonical: `https://www.nameideagenerator.com/blog/category/${slug}`,
+    alternates: {
+      canonical: `https://www.nameideagenerator.com/blog/category/${slug}`,
+    },
+  };
+}
+
+const Page = async ({ params }) => {
+  const slug = params.category;
   return <DynamicCategory slug={slug} />;
 };
 
-export default page;
+export default Page;
